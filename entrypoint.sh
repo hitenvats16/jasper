64867bd1f5ac3cef4f301ee075dc221b78c4d92e
Hiten Vats <hitenvats16@gmail.com>
2025-06-13 23:00:33 +0530

    added ffmpeg install command in shel script

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 if [ "$APP_NAME" = "gateway" ]; then
     python -m main
 elif [ "$APP_NAME" = "worker:voice_processor" ]; then
+    apt-get update && apt-get install -y ffmpeg
     python -m workers.voice_processor
 else
     echo "Invalid APP_NAME. Must be either 'gateway' or 'worker:voice_processor'"
